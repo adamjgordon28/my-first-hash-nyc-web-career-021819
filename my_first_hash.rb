06ac99c1e602b62fe3031eeedc5b14ccbff53b72
Adam Gordon <adamjgordon24@gmail.com>
2019-01-29 04:41:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  adams_hash = {"name" => "Adam Gordon", "age" => 28, "weight" => 240}
 
   
 end
